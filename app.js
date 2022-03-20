325d25c2187a7e1ddb50f64a19bd7f24f498e8a5
Anthony Anenye <hillstony29@gmail.com>
2022-03-20 10:36:15 +0100

    added new styles and animations using javascript

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
-function pageTransitions() {
+
+function pageTransitions(){
   //Button click active class
-  for (let i = 0; i < sectBtn.length; i++){
+  for(let i = 0; i < sectBtn.length; i++){
     sectBtn[i].addEventListener('click', function(){
       let currentBtn = document.querySelectorAll('.active-btn');
       currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-      this.className += 'active-btn';
+      this.className += ' active-btn';
     })
   }
-  
+
+  // section Active class
+  allSections.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if(id){
+      //remove sellected from the others btn
+      sectBtns.forEach((btn) => {
+        btn.classList.remove('active')
+      })
+      e.target.classList.add('active')
+
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove('active')
+      })
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  })
 }
 
-pageTransitions()
+pageTransitions();
